b523bb00fb5d895a6de428d7c2c8c7483e455d90
Your Name <aroseca15@gmail.com>
2021-06-26 16:36:31 -0700

    wrote first test for isValidChain.

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,21 +1,29 @@
 const Blockchain = require('./Blockchain');
-const Block =require('./Blocks');
+const Block = require('./Blocks');
 
 describe('Blockchain', () => {
-    let BC;
+    let BC, BC2;
 
     beforeEach(() => {
         BC = new Blockchain();
+        BC2 = new Blockchain();
     });
 
     it('starts with genesis block', () => {
-expect(BC.chain[0]).toEqual(Block.genesis());
+        expect(BC.chain[0]).toEqual(Block.genesis());
     });
 
     it('adds a new block', () => {
         const data = 'foo';
         BC.addBlock(data);
 
-        expect(BC.chain[BC.chain.length-1].data)
+        expect(BC.chain[BC.chain.length - 1].data)
     });
+
+    it('validates a valid chain', () => {
+        BC2.addBlock('foo');
+
+        expect(BC.isValidChain(BC2.chain)).toBe(true);
+    })
+
 });
