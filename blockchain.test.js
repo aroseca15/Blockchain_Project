b1c94714709bab64a102da2a83d320bb906e54f2
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-06-29 21:29:47 -0700

    Narrowed down location of bug. Error limited to chain validation.

--- blockchain.test.js
+++ blockchain.test.js
@@ -20,31 +20,6 @@ describe('Blockchain', () => {
         expect(BC.chain[BC.chain.length - 1].data).toEqual(data);
     });
 
-    // it('validates a valid chain', () => {
-    //     BC2.addBlock('foo');
-
-    //     expect(BC.isValidChain(BC2.chain)).toBe(true);
-    // });
-
-    // it('invalidates a chain with a corrupt genesis block', () => {
-    //     BC2.chain[0].data = 'Bad Data';
-
-    //     expect(BC.isValidChain(BC2.chain)).toBe(false);
-    // });
-
-    // it('invalidates a corrupt chain', () => {
-    //     BC2.addBlock('Hello');
-    //     BC2.chain[1].data = 'Bye Bye';
-
-    //     expect(BC.isValidChain(BC2.chain)).toBe(false);
-    // });
-
-
-    // it('invalidates a chain with a corrupt chain', () => {
-    //     BC2.addBlock('Valid');
-    //     BC2.chain[1].data = 'Corrupted';
-
-    //     expect(BC.isValidChain(BC2.chain)).toBe(false);
-    // })
+    
 
 });
