5b6733e4fe83971efe0f6ca55597f9be74c162f4
Your Name <aroseca15@gmail.com>
2021-07-09 00:00:16 -0700

    added script to  package json and get request

--- App/index.js
+++ App/index.js
@@ -3,3 +3,11 @@ const Blockchain = require('../Blockchain/BlockchainIndex');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const BC = new Blockchain();
+
+app.get('/blocks', (req, res) => {
+    res.json(BC.chain);
+})
+
+app.listen(HTTP_PORT, ()=> {
+    console.log(`I am listening on ${HTTP_PORT}`);
+})
