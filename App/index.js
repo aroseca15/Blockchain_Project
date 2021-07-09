e84f02f6ff9e82cb234684c7a3b0c79f74ca2142
Your Name <aroseca15@gmail.com>
2021-07-08 23:42:18 -0700

    Started app.js

--- App/index.js
+++ App/index.js
@@ -1,1 +1,5 @@
-// Prep
+const express = require('express');
+const Blockchain = require('../Blockchain/BlockchainIndex');
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const app = express();
+const BC = new Blockchain();
