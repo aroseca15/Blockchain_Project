ab30cef71e838b9078d4a662ccb5cf3d311281f1
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-07-11 18:55:18 -0700

    Created functional GET, POST requests. Functional redirect also.

--- App/index.js
+++ App/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+// const bodyParser = require('body-parser')
 const Blockchain = require('../Blockchain/BlockchainIndex');
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const BC = new Blockchain();
 
+app.use(express.json());
+
 app.get('/blocks', (req, res) => {
     res.json(BC.chain);
-})
+});
+
+app.post('/mine', (req, res) => {
+   const block = BC.addBlock(req.body.data);
+   console.log(`New Block Added: ${block.toString()}`);
+
+   res.redirect('/blocks');
+});
 
 app.listen(HTTP_PORT, ()=> {
     console.log(`I am listening on ${HTTP_PORT}`);
-})
+});
